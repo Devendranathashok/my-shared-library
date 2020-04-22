51b5dcc9984cf353cc885ff277df138e528ad7ee
KAMMALA <DEVENDRANATHASHOK.KAMMALA@t-systems.com>
2020-04-22 18:42:38 +0530

    modifications

--- osDetails.groovy
+++ osDetails.groovy
@@ -1,10 +1,10 @@
 node{
-     stage(osdetails){
-        sshagent(['ubuntucreds']) {
-    sh 'ssh -o StrictHostKeyChecking=no -l ${username}@${hostname} cat /etc/*-release '
+     stage('osdetails'){
+        sshagent(['remotehostcreds']) {
+    sh 'ssh -o StrictHostKeyChecking=no  $username@$hostname cat /etc/*-release '
 }
     }
-    stage (notification){
+    stage ('notification'){
     emailext( 
      attachLog: true,
      body: "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n More info at: ${env.BUILD_URL}",
